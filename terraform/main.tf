7d184c139e629f3da7143b9937ccd1c3fb125fd8
OhotinDY <134877421+OhotinDY@users.noreply.github.com>
2023-10-17 19:08:15 +0300

    Add files via upload

---
+++ terraform/main.tf
@@ -0,0 +1,75 @@
+# Описание облака
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
+
+# Описание доступа и токена
+provider "yandex" {
+  token     = "y0_AgAAAAAVgmc6AATuwQAAAADlQgazpEKF1v9fQWOZe_brUpNHyPH2VCo"
+  cloud_id  = "b1ggl8k3ceaee7m4b2oe"
+  folder_id = "b1goro5b9fvkh8ohdjot"
+}
+
+# Создание ВМ1
+resource "yandex_compute_instance" "vm-1" {
+  name = "web1"
+  platform_id = "standard-v3"
+  zone = var.zone1
+
+  resources {
+    core_fraction = 20
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd89dg1rq7uqslc6eigm"
+      size = 10
+    }
+  }
+
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("./meta.yml")}"
+  }
+}
+
+# Создание ВМ2
+resource "yandex_compute_instance" "vm-2" {
+  name = "web2"
+  platform_id = "standard-v3"
+  zone = var.zone1
+
+  resources {
+    core_fraction = 20
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd89dg1rq7uqslc6eigm"
+      size = 10
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("./meta.yml")}"
+  }
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "zone1" {
+type = string
+default = "ru-central1-a"
+}
+
+variable "zone2" {
+type = string
+default = "ru-central1-b"
+}
+
