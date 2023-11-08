33ca49db9c05c7e6bea02580992f5283b43af818
OhotinDY <134877421+OhotinDY@users.noreply.github.com>
2023-11-08 16:08:27 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,16 @@
+# Описание облака
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
+
+# Описание доступа и токена
+provider "yandex" {
+  token     = "xxxxxxxxxx"
+  cloud_id  = "b1ggl8k3ceaee7m4b2oe"
+  folder_id = "b1goro5b9fvkh8ohdjot"
+}
 
